cee7e262469833721c07a4872e25af5fd33cc25c
Nkosinathi Mathela <nkosinathi.mathela@gmail.com>
2021-01-20 19:14:10 +0200

    SPRINGBOOT APP RUNS AS A CONSOLE APP

--- src/main/java/com/wonderlabz/iq/roulette/RouletteApplication.java
+++ src/main/java/com/wonderlabz/iq/roulette/RouletteApplication.java
@@ -2,8 +2,10 @@ package com.wonderlabz.iq.roulette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RouletteApplication {
 
 	public static void main(String[] args) {
